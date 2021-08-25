75277efd847189ecf4bc3f8842b6023c940e2f15
Tifanny Rivera <trivera51580@gmail.com>
2021-08-25 08:07:25 -0600

    added script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let addToDoButton = document.getElementById("addToDo")
+let toDoContainer = document.getElementById('toDoContainer')
+let inputField = document.getElementById('inputField')
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p')
+})
