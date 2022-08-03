dbd8d307f6987a548d1f57d80c0b580d94de1b6e
Tifanny Rivera <trivera51580@gmail.com>
2022-08-03 15:17:38 -0600

    starting js file, added alert to enter a task if the input field is === 0

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-let addToDoButton = document.getElementById("addToDo");
-let toDoContainer = document.getElementById("toDoContainer");
-let inputField = document.getElementById("inputField");
 
-addToDoButton.addEventListener("click", function () {
-  var paragraph = document.createElement("p");
-  paragraph.classList.add("paragraph-styling");
-  paragraph.innerText = inputField.value;
-  toDoContainer.appendChild(paragraph);
-  inputField.value = "";
-  paragraph.addEventListener("click", function () {
-    paragraph.style.textDecoration = "line-through";
-  });
-  paragraph.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(paragraph);
-  });
-});
+document.querySelector('#push').onclick = function(){
+  if(document.querySelector('#newTask input').
+  value.length === 0){
+    alert('Please eneter a task!')
+  }
+}
+
+// let addToDoButton = document.getElementById("addToDo");
+// let toDoContainer = document.getElementById("toDoContainer");
+// let inputField = document.getElementById("inputField");
+
+// addToDoButton.addEventListener("click", function () {
+//   var paragraph = document.createElement("p");
+//   paragraph.classList.add("paragraph-styling");
+//   paragraph.innerText = inputField.value;
+//   toDoContainer.appendChild(paragraph);
+//   inputField.value = "";
+//   paragraph.addEventListener("click", function () {
+//     paragraph.style.textDecoration = "line-through";
+//   });
+//   paragraph.addEventListener("dblclick", function () {
+//     toDoContainer.removeChild(paragraph);
+//   });
+// });
