6695074123c0ded0d21ad02f022a3a01b7c93196
Tifanny Rivera <trivera51580@gmail.com>
2022-08-04 11:36:17 -0600

    cleaning up some of the code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newTask input").value.length === 0) {
     alert("Please eneter a task!");
   } else {
-    let hide = document.querySelector('.hide')
+    let hide = document.querySelector(".hide");
     hide.style.display = "block";
 
     document.querySelector("#tasks").innerHTML += `
@@ -22,13 +22,10 @@ document.querySelector("#push").onclick = function () {
         this.parentNode.remove();
       };
     }
-    // this function is to cross off tasks
-    // however, it will put a line through
-    // all tasks and not just one
 
     let tasks = document.querySelectorAll(".task");
     for (let i = 0; i < tasks.length; i++) {
-      tasks[i].onclick = function() {
+      tasks[i].onclick = function () {
         this.classList.toggle("completed");
       };
     }
