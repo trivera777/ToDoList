af4050934373ad6f70421377885254f37aa27235
Tifanny Rivera <trivera51580@gmail.com>
2022-08-03 19:57:38 -0600

    removed border around container

--- script.js
+++ script.js
@@ -2,34 +2,32 @@ document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newTask input").value.length === 0) {
     alert("Please eneter a task!");
   } else {
-    document.querySelector("#tasks").innerHTML +=
-      `
+    document.querySelector("#tasks").innerHTML += 
+    `
     <div class='task'>
-    <span id='taskName'>
-    ${document.querySelector("#newTask input").value}
-    </span>
+     <span id='taskName'>
+       ${document.querySelector("#newTask input").value}
+     </span>
     <button class='delete'>
-    <span class="icon-text has-text-danger">
-  <span class="icon">
-    <i class="fas fa-ban"></i>
-  </span>
-</span>
+    X
     </button>
     </div>
     `;
 
-    let currentTasks = document.querySelectorAll('.delete')
-    for(let i = 0; i < currentTasks.length; i ++){
-      currentTasks[i].onclick = function(){
-        this.parentNode.remove()
-      }
+    let currentTasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < currentTasks.length; i++) {
+      currentTasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
     }
 
-    let tasks = document.querySelectorAll('.tasks')
-    for (var i = 0; i < tasks.length; i++){
-      tasks[i].onclick = function(){
-        this.classList.toggle('completed')
-      }
+    let tasks = document.querySelectorAll(".tasks");
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
     }
+
+    document.querySelector("#newTask input").value = "";
   }
 };
